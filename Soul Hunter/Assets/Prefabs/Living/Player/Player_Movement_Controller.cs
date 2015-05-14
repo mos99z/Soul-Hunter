a43d2f7ce14410ec03300e97cecc4f2f79824738
mos99z <austinlware@gmail.com>
2015-05-14 05:51:30 -0400

    -User Stories Camera:      Follows Player, Orthographic View, Good Angle and Distance, Set as main.           *Need to add hiding walls* Player:      Movement, Sprite Placeholder, Facing Direction Indicator. Tutorial Level Added:      Walls, Floors, Void Zone. (All with placeholder Textures)
    Added:
         Layers - (Wall, Floor)
         Living/Direction Indicator.prefab
         UI/Main Camera.prefab
         Environment/Floors/Floor.prefab
         UI/Global Light.prefab
    Updated:
         Environment/Walls/Wall.prefab
         Living/Player/Player.prefab

--- Soul Hunter/Assets/Prefabs/Living/Player/Player_Movement_Controller.cs
+++ Soul Hunter/Assets/Prefabs/Living/Player/Player_Movement_Controller.cs
@@ -1,15 +1,59 @@
-﻿using UnityEngine;
+﻿#define MOVEPLAYER
+#define ROTATEPLAYER
+
+using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player_Movement_Controller : MonoBehaviour {
 
-	// Use this for initialization
+	public float Speed = 0.25f;
+	public GameObject DirectionIndicator = null;
+	public Animator Animations = null;
+	public LayerMask CursorColliders;
+
+	private Camera ScreenCamera = null;
+	private Vector3 currentVelocity = Vector3.zero;
+
 	void Start () {
-	
+		//Cursor.visible = false;
+		ScreenCamera = Camera.main;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void Update()
+	{
+		#if (ROTATEPLAYER)
+		if (ScreenCamera != null)
+		{
+			Ray MouseAim = ScreenCamera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit MousePosition;
+			Physics.Raycast(MouseAim,out MousePosition, 100.0f, CursorColliders);
+			DirectionIndicator.transform.LookAt(new Vector3(MousePosition.point.x, DirectionIndicator.transform.position.y, MousePosition.point.z));
+			
+			if(DirectionIndicator.transform.forward.z >= 0.0f)
+				Animations.Play("Player_Up_Idle");
+			else
+				Animations.Play("Player_Down_Idle");
+		}
+		#endif
+	}
+
+	void FixedUpdate ()
+	{
+		#if (MOVEPLAYER)
+		currentVelocity.x = Input.GetAxisRaw ("Horizontal");
+		currentVelocity.z = Input.GetAxisRaw ("Vertical");
+		if (currentVelocity.magnitude < 1.0f)
+		{
+			gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+		}
+		else
+		{
+			gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+			currentVelocity.Normalize ();
+			currentVelocity *= Speed;
+			transform.position += currentVelocity;
+		}
+		#endif
 	}
 }
--- Soul Hunter/Assets/UI/Camera_Follow_Object.cs
+++ Soul Hunter/Assets/UI/Camera_Follow_Object.cs
@@ -3,13 +3,45 @@ using System.Collections;
 
 public class Camera_Follow_Object : MonoBehaviour {
 
-	// Use this for initialization
+	public GameObject FollowTarget = null;
+	public float DistanceOffset = 14.0f;
+	public float HeightOffset = 14.0f;
+	public LayerMask ObjectsToHide;
+	
+	private GameObject lastCollided = null;
+
 	void Start () {
-	
+		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
+		
+		if (FollowTarget != null) {
+			Vector3 newPosition = transform.position;
+			newPosition.x = FollowTarget.transform.position.x;
+			newPosition.y = FollowTarget.transform.position.y + HeightOffset;
+			newPosition.z = FollowTarget.transform.position.z - DistanceOffset;
+			
+			transform.position = newPosition;
+			
+			Ray CameraToPlayer = new Ray(transform.position, (FollowTarget.transform.position - transform.position));
+			//Ray toPlayer = Camera.main.ScreenPointToRay(new Vector2(Camera.main.pixelWidth * 0.5f, Camera.main.pixelHeight * 0.5f));
+			RaycastHit ObjectInBetween;
+			Physics.Raycast(CameraToPlayer, out ObjectInBetween, (FollowTarget.transform.position - transform.position).magnitude, ObjectsToHide);
+			if (ObjectInBetween.collider)
+			{
+				if (ObjectInBetween.collider != lastCollided){
+					if (lastCollided != null)
+						lastCollided.GetComponent<Renderer>().enabled = true;
+					ObjectInBetween.collider.gameObject.GetComponent<Renderer>().enabled = false;
+					lastCollided = ObjectInBetween.collider.gameObject;
+				}
+			}
+			else if (lastCollided != null)
+			{
+				lastCollided.GetComponent<Renderer>().enabled = true;
+				lastCollided = null;
+			}
+		}
 	}
 }
