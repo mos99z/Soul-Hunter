c6809e266778ec84213ad884dc65ea7cfe4a371f
mos99z <austinlware@gmail.com>
2015-05-14 18:42:58 -0400

    Added:      Scenes/GameBrain.prefab      Scenes/GameBrain.cs - Access point for all the players main stats.                          - Contains 2 functions for each stat for ease of updates. A Mod function that will change the value by a positive or negative value, and a Set function to set the value of the stat to what ever you want: All auto update the player stat tracking variables.
    Put together the prefab for the Spell Database, Modify the spell that is attached to the Spell Database and click apply on the Spell Database it's self in order to update correctly. All assets related to a spell needs to be in it's file for orginization.

---
+++ Soul Hunter/Assets/Scenes/GameBrain.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameBrain : MonoBehaviour {
+
+	public int PlayerMaxHealth = 1000;
+	public int PlayerCurrHealth = 1000;
+	public int PlayerLivesLeft = 3;
+	public int SoulCount = 0;
+	// Level 0 is tutorial
+	public int CurrentLevel = 1;
+
+
+	//Tally Specific Info
+	public int NumEnemiesKilled = 0;
+	public int DamageTaken = 0;
+	public int TotalSoulCount = 0;
+	public int DeathCount = 0;
+	public double GameTime = 0.0;
+	public int NumCastedSpells = 0;
+	public GameObject SpellDatabase = null;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//The Spell Database it's self should not be active.
+		//Only in scene for ease of development.
+		SpellDatabase.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameTime += Time.deltaTime;
+	
+	}
+
+	void ModMaxHealth(int _value)
+	{
+		PlayerMaxHealth += _value;
+		if (PlayerMaxHealth < 0)
+			PlayerMaxHealth = 0;
+
+		if (PlayerCurrHealth > PlayerMaxHealth)
+			PlayerCurrHealth = PlayerMaxHealth;
+	}
+	void SetMaxHealth(int _NewMaxHealth)
+	{
+		PlayerMaxHealth = _NewMaxHealth;
+		if (PlayerMaxHealth < 0)
+			PlayerMaxHealth = 0;
+
+		if (PlayerCurrHealth > PlayerMaxHealth)
+			PlayerCurrHealth = PlayerMaxHealth;
+	}
+
+	void ModHealth(int _value)
+	{
+		if (_value < 0)
+			DamageTaken -= _value;
+
+		PlayerCurrHealth += _value;
+		if (PlayerCurrHealth > PlayerMaxHealth)
+			PlayerCurrHealth = PlayerMaxHealth;
+
+		else if (PlayerCurrHealth < 0)
+		{
+			DamageTaken += PlayerCurrHealth;
+			PlayerCurrHealth = 0;
+		}
+	}
+	void SetHealth(int _NewHealth)
+	{
+		if (_NewHealth < PlayerCurrHealth)
+			DamageTaken += PlayerCurrHealth - _NewHealth;
+
+		PlayerCurrHealth = _NewHealth;
+		if (PlayerCurrHealth > PlayerMaxHealth)
+			PlayerCurrHealth = PlayerMaxHealth;
+
+		else if (PlayerCurrHealth < 0)
+		{
+			DamageTaken += PlayerCurrHealth;
+			PlayerCurrHealth = 0;
+		}
+	}
+
+	void ModLivesLeft(int _value)
+	{
+		PlayerLivesLeft += _value;
+
+		if (_value < 0)
+			DeathCount -= _value;
+
+		if (PlayerLivesLeft < 0)
+		{
+			DeathCount += PlayerLivesLeft;
+			PlayerLivesLeft = 0;
+		}
+	}
+	void SetLivesLeft(int _NewLives)
+	{
+		if (_NewLives < PlayerLivesLeft)
+			DeathCount += PlayerLivesLeft - _NewLives;
+
+		PlayerLivesLeft = _NewLives;
+
+		if (PlayerLivesLeft < 0)
+		{
+			DeathCount += PlayerLivesLeft;
+			PlayerLivesLeft = 0;
+		}
+	}
+
+	void ModSouls(int _value)
+	{
+		SoulCount += _value;
+		if (_value > 0)
+			TotalSoulCount += _value;
+	}
+	void SetSouls(int _NewSouls)
+	{
+		SoulCount = _NewSouls;
+	}
+
+	//Tally Specific Info
+	void AddKill()
+	{
+		++NumEnemiesKilled;
+	}
+
+	void SpellCasted(string _SpellName)
+	{
+		++NumCastedSpells;
+		// In the works
+		//SpellDatabase.transform.FindChild (_SpellName).GetComponent<GUIText>().text.;
+	}
+}
