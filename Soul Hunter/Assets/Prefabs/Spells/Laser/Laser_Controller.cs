bf145a3758e4b10a6ae6e80a385621d87c6791df
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-06-12 17:02:06 -0400

    fog and laser sorta works

--- Soul Hunter/Assets/Prefabs/Spells/Laser/Laser_Controller.cs
+++ Soul Hunter/Assets/Prefabs/Spells/Laser/Laser_Controller.cs
@@ -4,10 +4,61 @@ using System.Collections.Generic;
 
 public class Laser_Controller : MonoBehaviour 
 {
-	void Start()
+	public GameObject mouseMarker;		// mouse marker from game brain
+	public float startHeight = 1.5f;	// where to overwrite the y-axis
+	public float duration = 0.3f;		// how long in seconds for spell to travel
+	public float damage = 15.1f;			// how much damage to deal
+	public float recoveryTime = 1.5f;	// how long for spell to cooldwon
+	public int maxHits = 4;				// how many times spelll can hit before dieing
+	
+	int hits = 0;		// keep track of the hits
+	Vector3 origPos;	// where the spell starts
+	Vector3 targetPos;	// where the spell ends
+
+	void Start () 
 	{
-		Vector3 spawn = Vector3.zero;
-		spawn.y += 1.0f;
-		transform.position = spawn;
+		Vector3 spawn = GameBrain.Instance.Player.transform.position;
+		spawn.y = startHeight;
+		transform.position = origPos = spawn;
+		if (mouseMarker == null)
+			mouseMarker = GameBrain.Instance.MouseMarker;
+		spawn = mouseMarker.transform.position;
+		spawn.y = startHeight;
+		targetPos = spawn;
+		
+		GameBrain.Instance.Player.SendMessage("SetRecoverTime", recoveryTime);
+		
+		if (GameBrain.Instance.FireLevel > 0)
+		{
+			switch (GameBrain.Instance.FireLevel)
+			{
+			case 1: damage = damage*2.0f - 0.1f; break;
+			case 2: damage = damage*3.0f - 0.2f; break;
+			case 3: damage = damage*4.0f - 0.3f; break;
+			}
+		}
+		StartCoroutine("MoveSpell", duration);
+	}
+	
+	IEnumerator MoveSpell(float time)
+	{
+		float currentTime = 0.0f;
+		do{
+			transform.position = Vector3.Lerp(origPos, targetPos, currentTime/time);
+			currentTime += Time.deltaTime;
+			yield return null;
+		} while (currentTime < time);
+		Destroy(gameObject, 0.1f);		// safety timer to kill self
+	}
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Enemy")
+		{
+			other.transform.SendMessage("TakeDamage", damage);
+			hits++;
+			if (hits >= maxHits)
+				Destroy(gameObject);
+		}
 	}
 }
