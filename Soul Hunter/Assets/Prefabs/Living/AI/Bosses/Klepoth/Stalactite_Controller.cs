f32e781bff97664ff16b8083321b1796e7cb212c
Steven Quast <smquast@gmail.com>
2015-06-12 14:51:36 -0400

    Klepoth has stalactites falling Kamikaze has animation

---
+++ Soul Hunter/Assets/Prefabs/Living/AI/Bosses/Klepoth/Stalactite_Controller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stalactite_Controller : MonoBehaviour {
+
+	public float StartingHeight = 30.0f;
+	public float FallSpeed = 0.75f;
+	public float Damage = 100.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		transform.position = new Vector3 (transform.position.x,StartingHeight,transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		transform.position -= new Vector3(0,FallSpeed,0);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			col.SendMessage("TakeDamage",Damage);
+			Destroy(gameObject);
+		}
+
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
