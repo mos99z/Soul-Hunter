8aa1399ebae66655029d3dad3e5ff29c9d8ee743
Joshua Stankiewicz <joshua.stankiewicz@yahoo.com>
2015-06-13 00:03:05 -0400

    poison cloud sorta works

--- Soul Hunter/Assets/Prefabs/Spells/Poision Cloud/Poision_Cloud_Controller.cs
+++ Soul Hunter/Assets/Prefabs/Spells/Poision Cloud/Poision_Cloud_Controller.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Poision_Cloud_Controller : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class Poision_Cloud_Controller : MonoBehaviour 
+{
+	public GameObject mouseMarker;		// mouse marker from game brain
+	public float duration = 2.0f;		// how long spell lasts
+	public float damage = 5.0f;			// how much damage it does
+	public float recoveryTime = 1.5f;	// how long for spell to recharge
 	
+	float timer = 0.0f;		// for ticking damage
+	
+	// TODO: enable this when blind is implemented
+	//	public GameObject blind;	// debuff to apply
+	void Start () 
+	{
+		if (mouseMarker == null)
+			mouseMarker = GameBrain.Instance.MouseMarker;
+		transform.position = mouseMarker.transform.position;
+		GameBrain.Instance.Player.SendMessage("SetRecoverTime", recoveryTime);
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+		duration -= Time.deltaTime;
+		if (duration <= 0.0f)
+			Destroy (gameObject);
+	}
 	
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Enemy")
+		{
+			other.transform.SendMessage("TakeDamage", damage);
+			// TODO: apply blind debuff
+		}
+	}
+	
+	void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Enemy")
+		{
+			timer += Time.deltaTime;
+			if (timer >= 0.5f)
+			{
+				timer = 0.0f;
+				other.transform.SendMessage("TakeDamage", damage);
+			}
+		}
 	}
 }
