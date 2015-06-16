0e4672f6311204aea0d7e6be127def033d85365f
Steven Quast <smquast@gmail.com>
2015-06-16 14:48:14 -0400

    Changed the credits scene anchors to work properly and to wait a few seconds before going back to the main menu. modified the tally screen to wait for a few seconds before allowing the player to continue modified the exit prompt to unpause the game when you exit the game and allow the loading screen modified the juggernaut so he will not be able to move through doors modified juggernaut script so that the juggernaut will always be chasing the player

--- Soul Hunter/Assets/UI/Credits/Credits_Controller.cs
+++ Soul Hunter/Assets/UI/Credits/Credits_Controller.cs
@@ -3,15 +3,52 @@ using System.Collections;
 
 public class Credits_Controller : MonoBehaviour {
 
+	bool phaseOne = false;
+	bool phaseTwo = false;
+	bool phaseThree = false;
+	public GameObject[] PhaseOneObjects;
+	public GameObject[] PhaseTwoObjects;
+	public GameObject[] PhaseThreeObjects;
+	public float TimeUntilPhase3 = 3.0f;
+	float currentTimer = 0.0f;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (Input.anyKey)
+		currentTimer += Time.deltaTime;
+		if (phaseOne == false && currentTimer >= (TimeUntilPhase3 * 0.33f)) 
+		{
+			phaseOne = true;
+			for (int i = 0; i < PhaseOneObjects.Length; i++) 
+			{
+				PhaseOneObjects[i].SetActive(true);
+			}
+		}
+
+		if (phaseTwo == false && currentTimer >= (TimeUntilPhase3 * 0.66f)) 
+		{
+			phaseTwo = true;
+			for (int i = 0; i < PhaseTwoObjects.Length; i++) 
+			{
+				PhaseTwoObjects[i].SetActive(true);
+			}
+		}
+
+		if (phaseThree == false && currentTimer >= TimeUntilPhase3) 
+		{
+			phaseThree = true;
+			for (int i = 0; i < PhaseThreeObjects.Length; i++) 
+			{
+				PhaseThreeObjects[i].SetActive(true);
+			}
+		}
+		if (phaseThree && Input.anyKey)
 		{
 			// save level as 0 to not break the save file
 			GameBrain.Instance.CurrentLevel = 0;
