1c1ab2040079f3f5678154e517d507156e463103
mos99z <austinlware@gmail.com>
2015-06-19 18:40:35 -0400

    Quick save

--- Soul Hunter/Assets/Prefabs/Debuffs/Blinded/Blinded_Controller.cs
+++ Soul Hunter/Assets/Prefabs/Debuffs/Blinded/Blinded_Controller.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class Blinded_Controller : MonoBehaviour {
 
 	bool checkonce = true;
-	bool blinded = false;
 	MonoBehaviour[] Scripts;
 	NavMeshAgent navigation;
 	Vector3 destination;
@@ -12,26 +11,29 @@ public class Blinded_Controller : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-		Scripts = transform.parent.GetComponents<MonoBehaviour>();
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (checkonce) {
-			if (transform.parent.GetComponent<Living_Obj> ().entType == Living_Obj.EntityType.Boss) {
+		if (checkonce)
+		{
+			if (transform.parent.GetComponent<Living_Obj> ().entType == Living_Obj.EntityType.Boss)
+			{
 				Destroy (gameObject);
 				return;
 			}
 
-			//Scripts = transform.parent.GetComponents<MonoBehaviour>();
+			Scripts = transform.parent.GetComponents<MonoBehaviour>();
+
 			for (int i = 0; i < Scripts.Length; i++)
 			{
-				Scripts[i].enabled = false;
+				if (i != 0)
+					Scripts[i].enabled = false;
 			}
 
 			navigation = transform.parent.GetComponent<NavMeshAgent>();
-			navigation.autoBraking = true;
 			destination = Random.insideUnitSphere * 7;
 			destination.y = 0;
 			destination += transform.position;
@@ -39,26 +41,28 @@ public class Blinded_Controller : MonoBehaviour {
 			checkonce = false;
 		}
 
-		Duration -= Time.deltaTime;
-		navigation.SetDestination (destination);
+		transform.parent.GetComponent<NavMeshAgent>().SetDestination (destination);
 		
 		
-		if (navigation.remainingDistance == 0) 
+		if (navigation.remainingDistance < 0.1f) 
 		{
 			destination = Random.insideUnitSphere * 7;
 			destination.y = 0;
 			destination += transform.position;
 		}
 
+		Duration -= Time.deltaTime;
 		if (Duration <= 0.0f)
 			Destroy (gameObject);
 	}
 
 	void OnDestroy()
 	{
-		for (int i = 0; i < Scripts.Length; i++)
-		{
-			Scripts[i].enabled = true;
-		}
+		if (transform.parent != null && Scripts != null)
+			for (int i = 0; i < Scripts.Length; i++)
+			{
+				if (Scripts[i] != null)
+					Scripts[i].enabled = true;
+			}
 	}
 }
